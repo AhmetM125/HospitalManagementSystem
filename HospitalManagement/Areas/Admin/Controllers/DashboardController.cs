298faced6d024926696a0e1435a3fcd61ab99577
Ahmet Yurdal <Ahmet_Yurdal_2000@hotmail.com>
2023-08-07 16:33:28 +0300

    first commit

---
+++ HospitalManagement/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalManagement.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            ViewData["TotalEmployee"] = 15;
+            ViewData["TotalDoctor"] = 15;
+            ViewData["TotalAppoint"] = 15;
+            ViewData["TotalPatient"] = 15;
+            ViewData["AvaibleRoom"] = 15;
+            ViewData["RemainRoom"] = 15;
+            return View();
+        }
+        public ActionResult TotalEmployee()
+		{
+            return View();
+		}
+    }
+}
